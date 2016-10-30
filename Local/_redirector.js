ccbe2c2cf36c0b42423c6157f1a411203ac2a5c6
Coolkids <s196774065@gmail.com>
2016-10-30 13:55:37 +0800

    update

--- Local/_redirector.js
+++ Local/_redirector.js
@@ -11,35 +11,28 @@ resp: false // 可选，true 表示替换 response body
 //Google服務轉國內鏡像
 //參考https://github.com/jiacai2050/gooreplacer
 {
-name: "http ajax/fonts >> 科大反代",
-from: /^http:\/\/(ajax|fonts)\.googleapis\.com\/(.*)$/,
-to: "http://$1.proxy.ustclug.org/$2",
-regex: true
-},
-{
 //https://servers.ustclug.org/index.php/2014/06/blog-googlefonts-speedup/
-name: "https ajax/fonts >> 科大反代",
-from: /^https:\/\/(ajax|fonts)\.googleapis\.com\/(.*)$/,
-to: "https://$1.proxy.ustclug.org/$2",
+name: "ajax/fonts >> 科大反代",
+from: /^(http|https):\/\/(ajax|fonts)\.googleapis\.com\/(.*)$/,
+to: "$1://$2.proxy.ustclug.org/$3",
 regex: true
 },
 {
 name: "themes >> 科大反代",
-from: /^https?:\/\/themes\.googleusercontent\.com\/(.*)$/,
-to: "https://google-themes.proxy.ustclug.org/$1",
+from: /^(http|https):\/\/themes\.googleusercontent\.com\/(.*)$/,
+to: "$1://google-themes.proxy.ustclug.org/$2",
 regex: true
 },
 {
 name: "fonts-gstatic >> 科大反代",
-from: /^https?:\/\/fonts\.gstatic\.com\/(.*)$/,
-to: "https://fonts-gstatic.proxy.ustclug.org/$1",
+from: /^(http|https):\/\/fonts\.gstatic\.com\/(.*)$/,
+to: "$1://fonts-gstatic.proxy.ustclug.org/$2",
 regex: true
 },
 {
-//新浪到百度JS库
-name: "sina > baidu JS Libs",
-from: "http://lib.sinaapp.com/js/jquery/*/*",
-to: "http://libs.baidu.com/jquery/$1/$2",
-wildcard: true
+name: "gravatar >> 科大反代",
+from: /^(http|https):\/\/secure\.gravatar\.com\/(.*)$/,
+to: "$1://gravatar.proxy.ustclug.org/$2",
+regex: true
 },
 ];
