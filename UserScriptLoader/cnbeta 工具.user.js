f6c95a47220a088646ca38b91955bbfa978a8db3
Coolkids <s196774065@gmail.com>
2017-02-24 11:11:02 +0800

    cnbeta

---
+++ UserScriptLoader/cnbeta 工具.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name       cnbeta 工具
+// @version    0.0.1
+// @description  cnbeta 工具
+// @include      http://*.cnbeta.com/*
+// @include      https://*.cnbeta.com/*
+// @require      http://code.jquery.com/jquery-2.1.1.min.js
+// @grant        unsafeWindow
+// @run-at       document-end
+// @copyright  2017+, Coolkid
+// ==/UserScript==
+(function() {
+    'use strict';
+    var a = function(){
+        var div = $("div[style*='important']");
+        //console.log(div.html());
+        div.remove();
+        var body = $("body");
+        body.css("padding-top","0px");
+    };
+    a();
+}).apply(unsafeWindow);
