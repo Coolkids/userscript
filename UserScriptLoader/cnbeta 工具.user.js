2d750335c9832f847d9b957253abfae8eb3b8a9f
Coolkids <s196774065@gmail.com>
2017-03-23 10:03:46 +0800

    update cnbeta

--- UserScriptLoader/cnbeta 工具.user.js
+++ UserScriptLoader/cnbeta 工具.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       cnbeta 工具
-// @version    0.0.5
+// @version    0.0.7
 // @description  cnbeta 工具
 // @include      http://*.cnbeta.com/*
 // @include      https://*.cnbeta.com/*
@@ -23,7 +23,7 @@
         img.each(function(){
             var newimg = $(this).attr("original");
             var oldimg = $(this).attr("src");
-            if(oldimg!=newimg&&newimg!=null){
+            if(oldimg!=newimg&&newimg!="null"){
                 console.log($(this).attr("src")+"替换为"+$(this).attr("original"));
                 $(this).attr("src", newimg);
             }
@@ -31,4 +31,16 @@
     };
     a();
     setTimeout(b, 1000);
+    var c = function(){
+        var alist = $("body > div.main-wrap > div.cnbeta-update > div > div.cnbeta-update-list > div.items-area a");
+        alist.each(function(){
+            var oldtitle = $(this).text();
+            var newtitle = oldtitle.replace(/^[^！]+！/, "");
+            if(oldtitle.indexOf("！")>-1){
+                $(this).text(newtitle);
+                console.log("原标题<"+oldtitle+">替换为<"+newtitle+">");
+            }
+        });
+    };
+    setTimeout(c, 1000);
 }).apply(unsafeWindow);
