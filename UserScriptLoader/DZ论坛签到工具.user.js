86710ca2a2860d0532fd8a169bf8dcc6c3dfa618
coolkids <s196774065@gmail.com>
2018-09-22 10:37:13 +0800

    更新签到

--- UserScriptLoader/DZ论坛签到工具.user.js
+++ UserScriptLoader/DZ论坛签到工具.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       DZ论坛签到工具
-// @version    0.2.6
+// @version    0.2.7
 // @description  自动签到工具
 // @include      http://*/plugin.php?id=dsu_paulsign*
 // @include      http://bbs.kafan.cn/*
@@ -12,68 +12,88 @@
 // @copyright  2017+, Coolkid
 // ==/UserScript==
 
-function isURL(x){
-    if(window.location.href.indexOf(x)!=-1){
-    	return true;
-    }else{
-     	return false;
-    }
-}
 
-function qd(){
-	if(window.find("今天签到了吗")&&window.find("请选择您此刻的心情图片并写下今天最想说的话")){
-		var text = document.getElementById("ch_s");
-		var text2 = document.getElementById("todaysay");
-        if(text==null){
-        	return;
+(function() {
+    'use strict';
+
+    let isURL = function(x){
+        if(window.location.href.indexOf(x)!=-1){
+            return true;
+        }else{
+            return false;
         }
-    	text.setAttribute('checked',true);    
-        text2.value = "全自动签到,全自动签到";
-		var button = document.getElementById("qiandao");
-		button.submit();
-        /*var inplen = document.getElementById("qiandao").getElementsByTagName("a");
+    };
+
+    let qd = function(){
+        if(window.find("今天签到了吗")&&window.find("请选择您此刻的心情图片并写下今天最想说的话")){
+            var text = document.getElementById("ch_s");
+            var text2 = document.getElementById("todaysay");
+            if(text==null){
+                return;
+            }
+            text.setAttribute('checked',true);
+            text2.value = "全自动签到,全自动签到";
+            var button = document.getElementById("qiandao");
+            button.submit();
+            /*var inplen = document.getElementById("qiandao").getElementsByTagName("a");
 		for (var i = 0, len = inplen.length; i < len; i++) {
 			if(inplen[i].href=="javascript:;"){
-				var ev = document.createEvent('HTMLEvents'); 
-				ev.initEvent('click', false, true); 
+				var ev = document.createEvent('HTMLEvents');
+				ev.initEvent('click', false, true);
 				inplen[i].dispatchEvent(ev);
 				return;
 			}
 		}*/
-        return;
+            return;
+        }
     }
-}
 
-function qd2(){
-	var imgs = document.getElementById("pper_a").getElementsByTagName("IMG");
-	if(imgs[0].src.indexOf("wb.png")==-1){
-		var a = document.getElementById("pper_a");;
-		/*var ev = document.createEvent('HTMLEvents'); 
-		ev.initEvent('click', false, true); 
+    let qd2 = function(){
+        var imgs = document.getElementById("pper_a").getElementsByTagName("IMG");
+        if(imgs[0].src.indexOf("wb.png")==-1){
+            var a = document.getElementById("pper_a");;
+            /*var ev = document.createEvent('HTMLEvents');
+		ev.initEvent('click', false, true);
 		a.dispatchEvent(ev);*/
-		a.onclick();
-		return;
-	}
-}
-
-
-if(isURL("tsdm")){
-	//天使论坛
-	qd();
-    if(window.find("签到领奖")&&(window.location.href!="http://www.tsdm.me/plugin.php?id=dsu_paulsign:sign")){
-        window.location.href="http://www.tsdm.me/plugin.php?id=dsu_paulsign:sign";
-        return;
+            a.onclick();
+            return;
+        }
     }
-}else if(isURL("kafan")){
-	//卡饭论坛
-	qd2();
-}else if(isURL("lightnovel")){
-	//轻国
-	if(window.find("每日任务")&&window.find("啪啪啪")){
-		window.location.href="http://www.lightnovel.cn/home.php?mod=task&do=apply&id=98";
-		return;
-	}
-}else{
-	//其他论坛
-	qd();
-}
+
+    let doit = function(){
+        if(isURL("tsdm")){
+            //天使论坛
+            qd();
+            console.log(window.find("签到领奖"), (window.location.href!="http://www.tsdm.me/plugin.php?id=dsu_paulsign:sign"));
+            if(window.find("签到领奖")&&(window.location.href!="http://www.tsdm.me/plugin.php?id=dsu_paulsign:sign")){
+                window.location.href="http://www.tsdm.me/plugin.php?id=dsu_paulsign:sign";
+                return;
+            }
+        }else if(isURL("kafan")){
+            //卡饭论坛
+            qd2();
+        }else if(isURL("lightnovel")){
+            //轻国
+            let a=false,b=false;
+            $(".bbda").each(function() {
+                console.log( $(this).text());
+                if ($(this).text().indexOf("每日任务")!=-1){
+                    a = true;
+                }
+            })
+             $(".bbda").each(function() {
+                if ($(this).text().indexOf("啪啪啪")!=-1){
+                    b=true;
+                }
+            })
+            if(a&&b){
+                window.location.href="http://www.lightnovel.cn/home.php?mod=task&do=apply&id=98";
+                return;
+            }
+        }else{
+            //其他论坛
+            qd();
+        }
+    }
+    doit();
+})();
